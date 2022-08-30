f1a6bfac47d409c8256e1722b5b0d6d7570e05de
YanHeng <yanh@siactpower.com>
2022-08-30 15:12:47 +0800

    build:新增packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,7 +1,16 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
+
+const r = (p: string) => resolve(__dirname, p)
 
 export default defineConfig({
   test: {
     exclude: ['**/node_modules/**', '**/dist/**', '**/.{idea,git,cache,output,temp}/**'],
+  },
+  resolve: {
+    alias: {
+      '@edoms/utils': r('./packages/utils'),
+      '@edoms/components': r('./packages/components'),
+    },
   },
 })
