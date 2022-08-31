d3a4a3ac93ec719b28f4c1b056d4a8fc66750a26
YanHeng <yanh@siactpower.com>
2022-08-31 14:27:15 +0800

    feat:集成element-plus

--- edoms-designer/src/util/request.ts
+++ edoms-designer/src/util/request.ts
@@ -3,6 +3,19 @@ import { Request } from '@edoms/utils'
 const request = new Request({
   baseURL: '',
   timeout: 1000 * 60 * 5,
+  headers: {
+    'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8',
+  },
+  interceptors: {
+    requestInterceptors: {
+      then(config) {
+        return config
+      },
+      catch(err) {
+        return Promise.reject(err)
+      },
+    },
+  },
 })
 
 export default request
