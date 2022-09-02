063003f73b8f3c3b6481582847d7b26527c0a8e9
YanHeng <yanh@siactpower.com>
2022-09-02 15:55:10 +0800

    test:测试currentDateRange方法

--- packages/utils/src/__test__/date.test.ts
+++ packages/utils/src/__test__/date.test.ts
@@ -1,6 +1,7 @@
 import dayjs from 'dayjs'
 import { describe, beforeEach, test, expect } from 'vitest'
 import {
+  currentDateRange,
   dateRange,
   formatCurrentDateRange,
   formatDate,
@@ -197,23 +198,234 @@ describe('date', () => {
   })
 
   test<LocalContext>('dateRange', ({ sample }) => {
-    const timestampResult = dateRange(sample.date, 'minute', true)
+    const minuteResult = dateRange(sample.date, 'minute', true)
 
-    const timestampTarget = {
+    const minuteTarget = {
       start: 1662008400000,
       end: 1662008459999,
       isTimestamp: true,
     }
 
-    expect(timestampResult).toEqual(timestampTarget)
+    expect(minuteResult).toEqual(minuteTarget)
 
-    const dateResult = dateRange(sample.date, 'minute')
+    const minuteDateResult = dateRange(sample.date, 'minute')
 
-    const dateTarget = {
-      start: dayjs(sample.date).startOf('minute').toDate(),
-      end: dayjs(sample.date).endOf('minute').toDate(),
+    const minuteDateTarget = {
+      start: new Date('2022-09-01 13:00:00'),
+      end: new Date('2022-09-01 13:00:59.999'),
     }
 
-    expect(dateResult).toEqual(dateTarget)
+    expect(minuteDateResult).toEqual(minuteDateTarget)
+
+    const hourResult = dateRange(sample.date, 'hour', true)
+
+    const hourTarget = {
+      start: 1662008400000,
+      end: 1662011999999,
+      isTimestamp: true,
+    }
+
+    expect(hourResult).toEqual(hourTarget)
+
+    const hourDateResult = dateRange(sample.date, 'hour')
+
+    const hourDateTarget = {
+      start: new Date('2022-09-01 13:00:00'),
+      end: new Date('2022-09-01 13:59:59.999'),
+    }
+
+    expect(hourDateResult).toEqual(hourDateTarget)
+
+    const dayResult = dateRange(sample.date, 'day', true)
+
+    const dayTarget = {
+      start: 1661961600000,
+      end: 1662047999999,
+      isTimestamp: true,
+    }
+
+    expect(dayResult).toEqual(dayTarget)
+
+    const dayDateResult = dateRange(sample.date, 'day')
+
+    const dayDateTarget = {
+      start: new Date('2022-09-01 00:00:00'),
+      end: new Date('2022-09-01 23:59:59.999'),
+    }
+
+    expect(dayDateResult).toEqual(dayDateTarget)
+
+    const monthResult = dateRange(sample.date, 'month', true)
+
+    const monthTarget = {
+      start: 1661961600000,
+      end: 1664553599999,
+      isTimestamp: true,
+    }
+
+    expect(monthResult).toEqual(monthTarget)
+
+    const monthDateResult = dateRange(sample.date, 'month')
+
+    const monthDateTarget = {
+      start: new Date('2022-09-01 00:00:00'),
+      end: new Date('2022-09-30 23:59:59.999'),
+    }
+
+    expect(monthDateResult).toEqual(monthDateTarget)
+
+    const weekResult = dateRange(sample.date, 'week', true)
+
+    const weekTarget = {
+      start: 1661616000000,
+      end: 1662220799999,
+      isTimestamp: true,
+    }
+
+    expect(weekResult).toEqual(weekTarget)
+
+    const weekDateResult = dateRange(sample.date, 'week')
+
+    const weekDateTarget = {
+      start: new Date('2022-08-28 00:00:00'),
+      end: new Date('2022-09-03 23:59:59.999'),
+    }
+
+    expect(weekDateResult).toEqual(weekDateTarget)
+
+    const yearResult = dateRange(sample.date, 'year', true)
+
+    const yearTarget = {
+      start: 1640966400000,
+      end: 1672502399999,
+      isTimestamp: true,
+    }
+
+    expect(yearResult).toEqual(yearTarget)
+
+    const yearDateResult = dateRange(sample.date, 'year')
+
+    const yearDateTarget = {
+      start: new Date('2022-01-01 00:00:00'),
+      end: new Date('2022-12-31 23:59:59.999'),
+    }
+
+    expect(yearDateResult).toEqual(yearDateTarget)
+  })
+
+  test<LocalContext>('currentDateRange', () => {
+    const minuteResult = currentDateRange('minute', true)
+
+    const minuteTarget = {
+      start: dayjs().startOf('minute').valueOf(),
+      end: dayjs().endOf('minute').valueOf(),
+      isTimestamp: true,
+    }
+
+    expect(minuteResult).toEqual(minuteTarget)
+
+    const minuteDateResult = currentDateRange('minute')
+
+    const minuteDateTarget = {
+      start: dayjs().startOf('minute').toDate(),
+      end: dayjs().endOf('minute').toDate(),
+    }
+
+    expect(minuteDateResult).toEqual(minuteDateTarget)
+
+    const hourResult = currentDateRange('hour', true)
+
+    const hourTarget = {
+      start: dayjs().startOf('hour').valueOf(),
+      end: dayjs().endOf('hour').valueOf(),
+      isTimestamp: true,
+    }
+
+    expect(hourResult).toEqual(hourTarget)
+
+    const hourDateResult = currentDateRange('hour')
+
+    const hourDateTarget = {
+      start: dayjs().startOf('hour').toDate(),
+      end: dayjs().endOf('hour').toDate(),
+    }
+
+    expect(hourDateResult).toEqual(hourDateTarget)
+
+    const dayResult = currentDateRange('day', true)
+
+    const dayTarget = {
+      start: dayjs().startOf('day').valueOf(),
+      end: dayjs().endOf('day').valueOf(),
+      isTimestamp: true,
+    }
+
+    expect(dayResult).toEqual(dayTarget)
+
+    const dayDateResult = currentDateRange('day')
+
+    const dayDateTarget = {
+      start: dayjs().startOf('day').toDate(),
+      end: dayjs().endOf('day').toDate(),
+    }
+
+    expect(dayDateResult).toEqual(dayDateTarget)
+
+    const monthResult = currentDateRange('month', true)
+
+    const monthTarget = {
+      start: dayjs().startOf('month').valueOf(),
+      end: dayjs().endOf('month').valueOf(),
+      isTimestamp: true,
+    }
+
+    expect(monthResult).toEqual(monthTarget)
+
+    const monthDateResult = currentDateRange('month')
+
+    const monthDateTarget = {
+      start: dayjs().startOf('month').toDate(),
+      end: dayjs().endOf('month').toDate(),
+    }
+
+    expect(monthDateResult).toEqual(monthDateTarget)
+
+    const weekResult = currentDateRange('week', true)
+
+    const weekTarget = {
+      start: dayjs().startOf('week').valueOf(),
+      end: dayjs().endOf('week').valueOf(),
+      isTimestamp: true,
+    }
+
+    expect(weekResult).toEqual(weekTarget)
+
+    const weekDateResult = currentDateRange('week')
+
+    const weekDateTarget = {
+      start: dayjs().startOf('week').toDate(),
+      end: dayjs().endOf('week').toDate(),
+    }
+
+    expect(weekDateResult).toEqual(weekDateTarget)
+
+    const yearResult = currentDateRange('year', true)
+
+    const yearTarget = {
+      start: dayjs().startOf('year').valueOf(),
+      end: dayjs().endOf('year').valueOf(),
+      isTimestamp: true,
+    }
+
+    expect(yearResult).toEqual(yearTarget)
+
+    const yearDateResult = currentDateRange('year')
+
+    const yearDateTarget = {
+      start: dayjs().startOf('year').toDate(),
+      end: dayjs().endOf('year').toDate(),
+    }
+
+    expect(yearDateResult).toEqual(yearDateTarget)
   })
 })
