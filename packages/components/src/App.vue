c1be484a9a209f82ca8addc2ccc52bc5c1f9b7f7
YanHeng <yanh@siactpower.com>
2022-09-05 11:00:31 +0800

    feat: 删除初始化组件

--- packages/components/src/App.vue
+++ packages/components/src/App.vue
@@ -1,23 +1,10 @@
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 </template>
 
-<script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import { object } from '@edoms/utils'
-
-const obj = {
-  name: 'Tome',
-}
-
-console.log(object.deepClone(obj))
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 header {
