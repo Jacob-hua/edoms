87637313bfe9655c7ec82c1b84953d2e02328cde
YanHeng <yanh@siactpower.com>
2022-09-06 17:28:16 +0800

    feat:edoms-designer增加404页面

---
+++ edoms-designer/src/layout/404.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <img :src="img" />
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: '404Vue',
+}
+</script>
+
+<script lang="ts" setup>
+import img from '@/assets/img/404.svg'
+</script>
+
+<style lang="scss" scoped>
+.container {
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+
+  img {
+    width: 1024 * 0.8px;
+    height: 1024 * 0.8px;
+  }
+}
+</style>
--- edoms-designer/src/router/index.ts
+++ edoms-designer/src/router/index.ts
@@ -1,18 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import AboutView from '@/views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
+      name: 'index',
       component: () => import('@/layout/index.vue'),
     },
     {
-      path: '/about',
-      name: 'about',
-      component: AboutView,
+      path: '/404',
+      name: '404',
+      component: () => import('@/layout/404.vue'),
     },
   ],
 })
