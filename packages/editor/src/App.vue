cd527acf35d3018a111321f6e4128bfb5b055131
YanHeng <yanh@siactpower.com>
2022-09-06 14:02:46 +0800

    feat:@edoms/editor集成@edoms/components

--- packages/editor/src/App.vue
+++ packages/editor/src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-
+    <el-button>ElementButton</el-button>
+    <EdComponent v-for="config in configs" :key="config.id" :node="config" />
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
     </div>
@@ -9,7 +10,29 @@
 </template>
 
 <script setup lang="ts">
+import { ref, Ref } from 'vue'
 import HelloWorld from './components/HelloWorld.vue'
+import { EdNode } from '@edoms/components'
+
+const configs: Ref<EdNode[]> = ref([
+  {
+    id: '0',
+    type: 'EdButton',
+    name: 'EDom',
+    text: '按钮',
+    disableText: '禁用按钮',
+  },
+  {
+    id: '1',
+    type: 'EdButton',
+    name: 'EDoms组件2',
+  },
+  {
+    id: '2',
+    type: 'EdButton',
+    name: 'EDoms组件22222',
+  },
+])
 </script>
 
 <style scoped>
--- packages/editor/src/main.ts
+++ packages/editor/src/main.ts
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import EdComponents from '@edoms/components'
 
 import './assets/main.css'
+import 'element-plus/theme-chalk/index.css'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+const { component, button, page } = EdComponents
+app.use(ElementPlus)
+app.component(component.name, component)
+app.component(button.name, button)
+app.component(page.name, page)
+app.mount('#app')
