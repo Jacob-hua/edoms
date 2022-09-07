21fe5184f40b1aaeed6f800a8d9513910e21d212
YanHeng <yanh@siactpower.com>
2022-09-07 09:17:31 +0800

    feat:删除无用组件，编辑router文件

--- edoms-designer/src/router/index.ts
+++ edoms-designer/src/router/index.ts
@@ -1,19 +1,39 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  NavigationGuardNext,
+  RouteLocationNormalized,
+  RouteRecordRaw,
+} from 'vue-router'
+
+const routes: RouteRecordRaw[] = [
+  {
+    path: '/',
+    name: 'index',
+    component: () => import('@/layout/index.vue'),
+    meta: {
+      title: '首页',
+    },
+  },
+  {
+    path: '/404',
+    name: '404',
+    component: () => import('@/layout/404.vue'),
+    meta: {
+      title: '404',
+    },
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'index',
-      component: () => import('@/layout/index.vue'),
-    },
-    {
-      path: '/404',
-      name: '404',
-      component: () => import('@/layout/404.vue'),
-    },
-  ],
+  routes,
+})
+
+router.beforeEach((to: RouteLocationNormalized, from: RouteLocationNormalized, next: NavigationGuardNext) => {
+  console.log(to.path)
+  console.log(from.path)
+  next()
 })
 
 export default router
