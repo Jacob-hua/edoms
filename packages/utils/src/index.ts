aba0cff9d8120aad6b87df9aa341092a66450e1b
YanHeng <yanh@siactpower.com>
2022-09-19 10:34:10 +0800

    feat: @edoms/engine抛出EventBus相关类

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -4,3 +4,6 @@ export * as object from './object'
 
 export * as date from './date'
 export * as dateType from './date/type'
+
+export { default as EventBus } from './event-bus'
+export * from './event-bus/type'
