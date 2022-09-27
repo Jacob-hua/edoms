632441e0423eee54cdf48e7bf2102f12152b2514
yanheng <yanh@siactpower.com>
2022-09-27 17:41:11 +0800

    feat: 新增@edoms/workshop项目提供编辑器核心类

--- packages/editor/src/App.vue
+++ packages/editor/src/App.vue
@@ -1,39 +1,10 @@
 <template>
   <header>
     <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
-    <el-button>ElementButton</el-button>
-    <EdComponent v-for="config in configs" :key="config.id" :node="config" />
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-    </div>
   </header>
 </template>
 
-<script setup lang="ts">
-import { ref, Ref } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-import { EdNode } from '@edoms/components'
-
-const configs: Ref<EdNode[]> = ref([
-  {
-    id: '0',
-    type: 'EdButton',
-    name: 'EDom',
-    text: '按钮',
-    disableText: '禁用按钮',
-  },
-  {
-    id: '1',
-    type: 'EdButton',
-    name: 'EDoms组件2',
-  },
-  {
-    id: '2',
-    type: 'EdButton',
-    name: 'EDoms组件22222',
-  },
-])
-</script>
+<script setup lang="ts"></script>
 
 <style scoped>
 header {
