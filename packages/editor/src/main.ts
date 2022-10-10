0cb14d9c643016aef15661862e365dc11147c760
yanheng <yanh@siactpower.com>
2022-10-10 11:02:22 +0800

    feat: @edoms/editor 无需集成 @edoms/components

--- packages/editor/src/main.ts
+++ packages/editor/src/main.ts
@@ -1,12 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import ElementPlus from 'element-plus'
-import EdomsComponents from '@edoms/components'
 
 import 'element-plus/theme-chalk/index.css'
 
 const app = createApp(App)
 
 app.use(ElementPlus)
-app.use(EdomsComponents)
 app.mount('#app')
