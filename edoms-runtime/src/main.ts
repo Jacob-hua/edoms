1866c5ce545546bef1543cae8a04c836984ec1ad
yanheng <yanh@siactpower.com>
2022-10-11 11:13:18 +0800

    fix: 修复element-plus启动访问不到zhCn

--- edoms-runtime/src/main.ts
+++ edoms-runtime/src/main.ts
@@ -5,7 +5,7 @@ import ElementPlus from 'element-plus'
 import EdomsComponents from '@edoms/components'
 
 import 'element-plus/theme-chalk/index.css'
-import { zhCn } from 'element-plus/es/locale'
+import zhCn from 'element-plus/es/locale/lang/zh-cn'
 
 const edomsApp = createApp(App)
 edomsApp.use(ElementPlus, {
