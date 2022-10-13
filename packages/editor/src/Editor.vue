39dd693eca279fdafb8ed53a04548fe4d53cfea7
yanheng <yanh@siactpower.com>
2022-10-13 13:52:32 +0800

    feat: 删除无用节点

--- packages/editor/src/Editor.vue
+++ packages/editor/src/Editor.vue
@@ -1,13 +1,6 @@
 <template>
-  <div>编辑器</div>
   <Workshop />
 </template>
-
-<!-- <script lang="ts">
-export default {
-  name: 'EdomsEditor',
-};
-</script> -->
 
 <script lang="ts" setup name="EdomsEditor">
 import Workshop from './components/Workshop.vue';
