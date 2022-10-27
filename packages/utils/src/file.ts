9b87e5f518e3e0aca3410263da3d88efd59aa9d5
yanheng <yanh@siactpower.com>
2022-10-27 11:51:29 +0800

    feat: @edoms/utils增加file相关工具

--- packages/utils/src/file.ts
+++ packages/utils/src/file.ts
@@ -5,3 +5,40 @@
 export const getFileExtension = (fileName: string): string => {
   return fileName.match(/\.([0-9a-z]+)(?:[\\?#]|$)/i)![1] ?? '';
 };
+
+/**
+ * 选择文件
+ *
+ * @param accepts
+ * @param multiple
+ * @returns
+ */
+export const selectFile = (accepts: string[] = ['.xml'], multiple: boolean = false): Promise<File[]> => {
+  if (!globalThis.document || !(globalThis.document instanceof Document)) {
+    throw new Error('This is not a browser environment');
+  }
+  const inputElem = globalThis.document.createElement('input');
+  inputElem.setAttribute('type', 'file');
+  inputElem.setAttribute('visibility', 'hidden');
+  if (Array.isArray(accepts) && accepts.length > 0) {
+    inputElem.setAttribute('accept', accepts.join(','));
+  }
+  if (multiple) {
+    inputElem.setAttribute('multiple', 'true');
+  }
+  inputElem.click();
+  return new Promise((resolve, reject) => {
+    inputElem.addEventListener('change', () => {
+      if (!inputElem.files || inputElem.files?.length == 0) {
+        reject();
+      } else {
+        const fileList: File[] = [];
+        for (let index = 0; index < inputElem.files.length; index++) {
+          const file = inputElem.files[index];
+          fileList.push(file);
+        }
+        resolve(fileList);
+      }
+    });
+  });
+};
