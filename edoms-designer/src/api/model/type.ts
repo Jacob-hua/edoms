51ff99d7edd1ed925fad0ed1910d563766ca80b9
yanheng <yanh@siactpower.com>
2022-11-14 10:25:36 +0800

    feat: 增加获取点位的API

--- edoms-designer/src/api/model/type.ts
+++ edoms-designer/src/api/model/type.ts
@@ -1,3 +1,5 @@
+import type { ModelCodeType, ModelPropType } from '@/const/model';
+
 export interface InstanceItem {
   insCode: string;
   insName: string;
@@ -11,4 +13,15 @@ export interface ListInstanceReq {
   virtual: string;
   deviceCode: string;
   isQueryDevice: boolean;
+}
+
+export interface PointItem {
+  label: string;
+  code: string;
+}
+
+export interface ListPointReq {
+  insId: string;
+  codeType: ModelCodeType;
+  propType: ModelPropType;
 }
