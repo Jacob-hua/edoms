533a05f3ce109e0234ce5c4bfbfdd8854c35e48a
yanheng <yanh@siactpower.com>
2022-11-14 14:09:39 +0800

    feat: 增加共通的内容导出钩子

---
+++ edoms-designer/src/const/mime.ts
@@ -0,0 +1,18 @@
+export const enum MimeType {
+  /** 文本 */
+  HTML = 'text/html',
+  CSS = 'text/css',
+  /** 图像 */
+  JPEG = 'image/jpeg',
+  PNG = 'image/png',
+  GIF = 'image/gif',
+  /** 视频 */
+  MPEG = 'audio/mpeg',
+  OGG = 'audio/ogg',
+  MP4 = 'video/mp4',
+  JSON = 'application/json',
+  JAVASCRIPT = 'application/javascript',
+  ECMASCRIPT = 'application/ecmascript',
+  /** office */
+  EXCEL = 'application/vnb.ms-excel',
+}
