b31d48c4422ac6bcf4cd0186ae3e1358488a3531
yanheng <yanh@siactpower.com>
2022-11-16 11:50:32 +0800

    feat: 动环监测组件支持等比例缩放，缩放时内容也会缩放

--- packages/ui/src/dynamic-monitoring/src/index.vue
+++ packages/ui/src/dynamic-monitoring/src/index.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="business-card">
-    <section class="header">
+  <div ref="scaleWrapper" class="business-card">
+    <div class="header">
       <div class="title">动环监测</div>
       <div class="subtitle">DYNAMIC MONITORING</div>
       <div class="operation">></div>
-    </section>
-    <section class="body">
+    </div>
+    <div class="body">
       <div class="dynamic-monitoring">
         <div v-for="({ icon, parameter, label }, index) in indicators" :key="index">
           <img :src="icon" />
@@ -13,12 +13,12 @@
           <div>{{ label }}</div>
         </div>
       </div>
-    </section>
+    </div>
   </div>
 </template>
 
 <script lang="ts" setup>
-import { ref, watchEffect } from 'vue';
+import { onMounted, ref, watchEffect } from 'vue';
 
 import { MDynamicMonitoring, MEnvironmentIndicator, MIndicatorItemConfig } from '../../types';
 import useApp from '../../useApp';
@@ -41,17 +41,39 @@ const props = defineProps<{
 
 useApp(props);
 
-const getIconByIndicatorType = (type: MEnvironmentIndicator) => {
-  const iconClassify = {
-    [MEnvironmentIndicator.GAS]: GasImg,
-    [MEnvironmentIndicator.LIQUID_DEPTH]: LiquidDepthImg,
-    [MEnvironmentIndicator.MOISTURE]: MoistureImg,
-    [MEnvironmentIndicator.TEMPERATURE]: TemperatureImg,
-  };
-  return iconClassify[type];
-};
-
 const indicators = ref<Indicator[]>([]);
+const scaleWrapper = ref<Element | null>(null);
+const wrapperWidth = 320;
+const wrapperHeight = 160;
+
+onMounted(() => {
+  if (!scaleWrapper.value) {
+    return;
+  }
+  new ResizeObserver((entries: ResizeObserverEntry[]) => {
+    const [wrapperEntry] = entries;
+    // const targetWidth = wrapperEntry.contentRect.width;
+    // const targetHeight = targetWidth / scale;
+
+    const widthScale = (wrapperEntry.contentRect.width * 1.0) / wrapperWidth;
+    const heightScale = (wrapperEntry.contentRect.height * 1.0) / wrapperHeight;
+    const scale = Math.min(widthScale, heightScale);
+    const originStyle = wrapperEntry.target.getAttribute('style');
+    wrapperEntry.target.setAttribute(
+      'style',
+      `aspect-ratio:${wrapperWidth / wrapperHeight};${originStyle};height: auto;`
+    );
+    Array.from(wrapperEntry.target.children).forEach((element: Element) => {
+      element.setAttribute(
+        'style',
+        `
+          transform:scale(${scale});
+          transform-origin: top left;
+        `
+      );
+    });
+  }).observe(scaleWrapper.value);
+});
 
 watchEffect(() => {
   indicators.value = props.config.indicators?.reduce(
@@ -62,6 +84,16 @@ watchEffect(() => {
     [] as Indicator[]
   );
 });
+
+function getIconByIndicatorType(type: MEnvironmentIndicator) {
+  const iconClassify = {
+    [MEnvironmentIndicator.GAS]: GasImg,
+    [MEnvironmentIndicator.LIQUID_DEPTH]: LiquidDepthImg,
+    [MEnvironmentIndicator.MOISTURE]: MoistureImg,
+    [MEnvironmentIndicator.TEMPERATURE]: TemperatureImg,
+  };
+  return iconClassify[type];
+}
 </script>
 
 <style lang="scss" scoped>
@@ -69,7 +101,7 @@ watchEffect(() => {
   display: flex;
   flex-direction: column;
   min-width: 320px;
-  min-height: 130px;
+  min-height: 160px;
 }
 
 .header {
