1a01441ec0c3512b9cb1255897809ff07c01b7e0
yanheng <yanh@siactpower.com>
2022-11-29 20:19:35 +0800

    update: 修改edoms-runtime打包配置

--- edoms-runtime/vite.config.ts
+++ edoms-runtime/vite.config.ts
@@ -5,7 +5,7 @@ import legacy from '@vitejs/plugin-legacy';
 import vue from '@vitejs/plugin-vue';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 // @ts-ignore
-import externalGlobals from 'rollup-plugin-external-globals';
+// import externalGlobals from 'rollup-plugin-external-globals';
 
 export default defineConfig(({ mode }) => {
   const [type, isProduction] = mode.split(':');
@@ -42,7 +42,7 @@ export default defineConfig(({ mode }) => {
         legacy({
           targets: ['defaults', 'not IE 11'],
         }),
-        externalGlobals({ vue: 'Vue' }, { exclude: [`./${type}/index.html`] }),
+        // externalGlobals({ vue: 'Vue' }, { exclude: [`./${type}/index.html`] }),
       ],
 
       root: `./${type}/`,
@@ -55,7 +55,7 @@ export default defineConfig(({ mode }) => {
         emptyOutDir: true,
         sourcemap: true,
         rollupOptions: {
-          external: ['vue'],
+          // external: ['vue'],
           input: path.resolve(process.cwd(), `./${type}/index.html`),
         },
         outDir,
