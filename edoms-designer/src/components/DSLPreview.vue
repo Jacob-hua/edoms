28634701dbb0c6b248e402bbd60f9cc2cded8f11
yanheng <yanh@siactpower.com>
2022-11-29 17:01:47 +0800

    feat: 新增DSLPreview组件

---
+++ edoms-designer/src/components/DSLPreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <iframe :width="width" :height="height" :src="previewUrl"></iframe>
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+withDefaults(
+  defineProps<{
+    contentId: string;
+    width?: string | number;
+    height?: string | number;
+  }>(),
+  {
+    width: () => '100%',
+    height: () => 'auto',
+  }
+);
+
+const { VITE_RUNTIME_PATH } = import.meta.env;
+
+const previewUrl = computed(() => `${VITE_RUNTIME_PATH}/page/index.html?localPreview=1`);
+</script>
