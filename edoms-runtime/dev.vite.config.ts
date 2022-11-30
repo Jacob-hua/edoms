a0e34e3afff0f6022af30bc7df5258481ee32827
yanheng <yanh@siactpower.com>
2022-11-30 10:46:35 +0800

    build: 调整运行时依赖配置

--- edoms-runtime/dev.vite.config.ts
+++ edoms-runtime/dev.vite.config.ts
@@ -16,6 +16,11 @@ export default defineConfig(({ mode }) => {
         { find: /^@edoms\/utils/, replacement: path.join(__dirname, '../packages/utils/src/index.ts') },
         { find: /^@edoms\/core/, replacement: path.join(__dirname, '../packages/core/src/index.ts') },
         { find: /^@edoms\/schema/, replacement: path.join(__dirname, '../packages/schema/src/index.ts') },
+        { find: /^@edoms\/design/, replacement: path.join(__dirname, '../packages/design/src/index.ts') },
+        {
+          find: /^@edoms\/element-plus-adapter/,
+          replacement: path.join(__dirname, '../packages/element-plus-adapter/src/index.ts'),
+        },
       ],
     },
 
