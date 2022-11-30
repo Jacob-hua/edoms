a5a08234f011ea93df61bcae64e0af7752d98464
yanheng <yanh@siactpower.com>
2022-11-30 10:48:28 +0800

    refactor: 组件DSLPreview增加必填属性applicationId

--- edoms-designer/src/views/page-editor/component/DSLPreviewDialog.vue
+++ edoms-designer/src/views/page-editor/component/DSLPreviewDialog.vue
@@ -1,6 +1,6 @@
 <template>
   <el-dialog v-model="dialogVisible" destroy-on-close class="pre-viewer" title="预览" :width="width">
-    <DSLPreview :height="stageRect.height" :content-id="contentId" />
+    <DSLPreview :height="stageRect.height" application-id="1" :content-id="contentId" />
   </el-dialog>
 </template>
 
