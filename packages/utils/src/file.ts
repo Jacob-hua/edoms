6621842c1a17c3cfb2d2f51d4c2d8afe91e70e86
yanheng <yanh@siactpower.com>
2022-12-02 10:33:49 +0800

    refactor: 改良selectFile中的类型转换

--- packages/utils/src/file.ts
+++ packages/utils/src/file.ts
@@ -43,12 +43,7 @@ export const selectFile = (accepts: string[] = ['.xml'], multiple: boolean = fal
       if (!inputElem.files || inputElem.files?.length == 0) {
         reject();
       } else {
-        const fileList: File[] = [];
-        for (let index = 0; index < inputElem.files.length; index++) {
-          const file = inputElem.files[index];
-          fileList.push(file);
-        }
-        resolve(fileList);
+        resolve(Array.from(inputElem.files));
       }
     });
   });
