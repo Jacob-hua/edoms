ade649bc52d47abc6a33a0bd733dbeca5ad8f42b
yanheng <yanh@siactpower.com>
2022-12-03 15:05:04 +0800

    feat: 设计器增加保存为版本按钮

--- edoms-designer/src/api/version/index.ts
+++ edoms-designer/src/api/version/index.ts
@@ -10,6 +10,54 @@ import { request } from '@/util/request';
 
 export * from './type';
 
+export default {
+  listVersions: async (data: ListVersionsReq): Promise<ListVersionsRes> => {
+    try {
+      const { result } = await request<ListVersionsReq, ListVersionsRes>({
+        url: '/page/version/list',
+        method: 'POST',
+        data,
+      });
+      return result;
+    } catch (e) {
+      return {
+        count: 0,
+        limit: 0,
+        page: 0,
+        dataList: [],
+      };
+    }
+  },
+  updateVersion: async (data: UpdateVersionReq): Promise<void> => {
+    await request<UpdateVersionReq, void>({
+      url: '/page/version/update',
+      method: 'PUT',
+      data,
+    });
+  },
+  saveWithVersion: async (data: SaveWithVersionReq): Promise<void> => {
+    await request<SaveWithVersionReq, void>({
+      url: '/page/version/save-with-version',
+      method: 'POST',
+      data,
+    });
+  },
+  recoveryVersion: async (data: RecoveryVersionReq): Promise<void> => {
+    await request<RecoveryVersionReq, void>({
+      url: 'page/version/version-recovery',
+      method: 'POST',
+      data,
+    });
+  },
+  deleteVersion: async (data: DeleteVersion): Promise<void> => {
+    await request<DeleteVersion, void>({
+      url: '/page/version/delete',
+      method: 'DELETE',
+      data,
+    });
+  },
+};
+
 export const listVersions = async (data: ListVersionsReq): Promise<ListVersionsRes> => {
   try {
     const { result } = await request<ListVersionsReq, ListVersionsRes>({
