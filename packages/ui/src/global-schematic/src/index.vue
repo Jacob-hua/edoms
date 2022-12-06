87701c03ead813f76ba2be90340b2643aaf4f4fd
yanheng <yanh@siactpower.com>
2022-12-06 16:50:44 +0800

    feat: 全局示意图增加图片上传组件

--- packages/ui/src/global-schematic/src/index.vue
+++ packages/ui/src/global-schematic/src/index.vue
@@ -1,9 +1,9 @@
 <template>
-  <img :src="imgSrc" />
+  <img ref="imgRef" :src="imgSrc" />
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 
 import useApp from '../../useApp';
 
@@ -15,15 +15,30 @@ const props = defineProps<{
 
 useApp(props);
 
+const imgRef = ref<HTMLImageElement>();
+
 const imgSrc = ref(props.config.src);
 
 watch(
   () => props.config.src,
-  (value) => {
-    imgSrc.value = value;
+  (value: any) => {
+    if (!value) {
+      imgSrc.value = 'https://puui.qpic.cn/vupload/0/1573555382625_bhp0wud8l6w.png/0';
+      return;
+    }
+
+    const url = value[0].url;
+    // TODO: 此处应该优先处理离线时的url，将设计时的url作为候补
+    imgSrc.value = `http://k8s.isiact.com/edoms-designtime-service-dev/edoms/design-time/file/preview/?contentId=${url}`;
   },
   {
     immediate: true,
   }
 );
+
+onMounted(() => {
+  imgRef.value?.addEventListener('error', (event) => {
+    console.log(event, '===');
+  });
+});
 </script>
