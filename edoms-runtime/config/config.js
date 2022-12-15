c633cf11aad05bef077a82807560697ab2fe68ff
yanheng <yanh@siactpower.com>
2022-12-15 16:28:13 +0800

    build: 调整运行时打包配置

--- edoms-runtime/config/config.js
+++ edoms-runtime/config/config.js
@@ -1,5 +1,10 @@
 const VITE_CONFIG = {
-  baseApi: 'http://localhost:8890/runtime-api/edoms/run-time',
+  dev: {
+    baseApi: 'http://localhost:8890/runtime-api/edoms/run-time',
+  },
+  admin: {
+    baseApi: 'http://k8s.isiact.com/edoms-runtime-service-dev/edoms/run-time',
+  },
 };
 
 window.VITE_CONFIG = VITE_CONFIG;
--- edoms-runtime/page/utils/request.ts
+++ edoms-runtime/page/utils/request.ts
@@ -2,7 +2,7 @@ import { App } from 'vue';
 
 import { ContentType, EdomsRequestConfig, EdomsRequestFunc, EdomsResponseData, Request } from '@edoms/utils';
 
-console.log('====window.VITE_CONFIG', (window as any).VITE_CONFIG);
+console.log('====window.VITE_CONFIG', import.meta.env.MODE, (window as any).VITE_CONFIG);
 const service = new Request({
   baseURL: import.meta.env.VITE_BASE_API ?? 'http://k8s.isiact.com/edoms-runtime-service-dev/edoms/run-time',
   timeout: 1000 * 10,
