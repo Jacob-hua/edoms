3308fe6720c4036fc4eea5c665c3c43218f4ef17
yanheng <yanheng@siactpower.com>
2022-12-22 20:06:48 +0800

    feat: 调整登陆页面样式

--- edoms-designer/src/views/login/index.vue
+++ edoms-designer/src/views/login/index.vue
@@ -1,46 +1,66 @@
 <template>
   <div class="wrapper">
     <section v-show="loginFormVisible" class="form-section">
-      <h2>登录</h2>
-      <el-form ref="loginFormRef" :model="loginFormModel" :rules="loginFormRules">
-        <el-form-item label="用户名" prop="username">
-          <el-input
-            v-model="loginFormModel.username"
-            type="text"
-            name="username"
-            oninvalid="setCustomValidity('请输入用户名');"
-          />
-        </el-form-item>
+      <div class="title">E-DOMS</div>
+      <div class="body">
+        <el-form
+          ref="loginFormRef"
+          size="large"
+          label-width="80px"
+          label-position="left"
+          :model="loginFormModel"
+          :rules="loginFormRules"
+        >
+          <el-form-item label="用户名" prop="username">
+            <el-input
+              v-model="loginFormModel.username"
+              type="text"
+              name="username"
+              oninvalid="setCustomValidity('请输入用户名');"
+            />
+          </el-form-item>
 
-        <el-form-item label="密码" prop="password">
-          <el-input
-            v-model="loginFormModel.password"
-            type="password"
-            name="password"
-            oninvalid="setCustomValidity('请输入登陆密码');"
-          />
-        </el-form-item>
-
+          <el-form-item label="密  码" prop="password">
+            <el-input
+              v-model="loginFormModel.password"
+              type="password"
+              name="password"
+              oninvalid="setCustomValidity('请输入登陆密码');"
+            />
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="footer">
         <el-button type="primary" @click="nextStep">下一步</el-button>
-      </el-form>
+      </div>
     </section>
     <section v-show="tenantFormVisible" class="form-section">
-      <h2>选择租户</h2>
-      <el-form ref="tenantFormRef" :model="tenantFormModel" :rules="tenantFormRules">
-        <el-form-item label="租户" prop="tenantId">
-          <el-select v-model="tenantFormModel.tenantId">
-            <el-option
-              v-for="{ tenantId, tenantName } in accountStore.tenants"
-              :key="tenantId"
-              :value="tenantId"
-              :label="tenantName"
-            />
-          </el-select>
-        </el-form-item>
-
+      <div class="title">E-DOMS</div>
+      <div class="body">
+        <el-form
+          ref="tenantFormRef"
+          size="large"
+          label-width="80px"
+          label-position="left"
+          :model="tenantFormModel"
+          :rules="tenantFormRules"
+        >
+          <el-form-item label="租户" prop="tenantId">
+            <el-select v-model="tenantFormModel.tenantId">
+              <el-option
+                v-for="{ tenantId, tenantName } in accountStore.tenants"
+                :key="tenantId"
+                :value="tenantId"
+                :label="tenantName"
+              />
+            </el-select>
+          </el-form-item>
+        </el-form>
+      </div>
+      <div class="footer">
         <el-button type="primary" @click="backPreStep">上一步</el-button>
         <el-button type="primary" @click="login">登录</el-button>
-      </el-form>
+      </div>
     </section>
   </div>
 </template>
@@ -129,6 +149,41 @@ const login = () => {
 .form-section {
   background-color: aliceblue;
   width: 500px;
-  height: 500px;
+  height: 380px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+
+  .title {
+    height: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-size: 40px;
+    font-weight: 400;
+  }
+
+  .body {
+    flex: 1;
+    padding: 20px;
+    .el-select--large {
+      line-height: 40px;
+      width: 100%;
+    }
+  }
+
+  .footer {
+    height: 60px;
+    display: flex;
+    flex-direction: row;
+    & > button {
+      flex: 1;
+      height: 100%;
+      border-radius: 0;
+    }
+    .el-button + .el-button {
+      margin-left: 5px;
+    }
+  }
 }
 </style>
