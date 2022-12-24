bfb0bf04a2a93aa36070f9b9b7f58cee00c0fb3b
yanheng <yanheng@siactpower.com>
2022-12-24 12:57:57 +0800

    feat: 运行参数组件增加头部选择tab

--- packages/ui/src/running-parameters/src/index.vue
+++ packages/ui/src/running-parameters/src/index.vue
@@ -1,11 +1,26 @@
 <template>
-  <div>参数曲线</div>
+  <BusinessCard title="运行参数" min-width="480" min-height="200">
+    <template #operation>
+      <el-tabs v-model="activeTab" class="demo-tabs" @tab-click="handleTriggerTab">
+        <el-tab-pane label="系统曲线" name="first" />
+        <el-tab-pane label="设备曲线" name="second" />
+      </el-tabs>
+    </template>
+  </BusinessCard>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+import BusinessCard from '../../BusinessCard.vue';
+
 import { MRunningParameters } from './type';
 
 defineProps<{
   config: MRunningParameters;
 }>();
+
+const activeTab = ref<string>('');
+
+const handleTriggerTab = () => {};
 </script>
