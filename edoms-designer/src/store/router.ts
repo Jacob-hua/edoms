c1c59112bc42901adafff08fddd87ed4de602ad0
guoyuanfang <guoyf@siactpower.com>
2023-01-06 17:57:02 +0800

    fix: 路由报错及打包失败问题修改

--- edoms-designer/src/store/router.ts
+++ edoms-designer/src/store/router.ts
@@ -1,11 +1,11 @@
-import { RouteLocationNormalized, RouteRecordName } from 'vue-router';
+import { RouteLocationNormalized } from 'vue-router';
 import { defineStore, StoreDefinition } from 'pinia';
 
 interface State {
-  cacheComps: Set<RouteRecordName>;
+  cacheComps: Set<string>;
 }
 interface Getters {
-  getCacheComps: (state: State) => Set<string>;
+  getCacheComps: (state: State) => string[];
 }
 interface Action {
   cacheRouter: (from: RouteLocationNormalized, to: RouteLocationNormalized) => void;
@@ -15,15 +15,15 @@ type RoutersStoreType = StoreDefinition<string, State, Getters, Action>;
 
 const useRoutersStore: RoutersStoreType = defineStore('router', {
   state: (): State => ({
-    cacheComps: new Set<RouteRecordName>(),
+    cacheComps: new Set<string>(),
   }),
   actions: {
     cacheRouter(from: RouteLocationNormalized, to: RouteLocationNormalized) {
       if (Array.isArray(from.meta.leaveCaches) && from.meta.leaveCaches.includes(to.path)) {
-        from.name && this.cacheComps.add(from.name);
+        from.name && this.cacheComps.add(from.name as string);
       }
       if (Array.isArray(to.meta.leaveCaches) && !to.meta.leaveCaches.includes(from.path)) {
-        to.name && this.cacheComps.delete(to.name);
+        to.name && this.cacheComps.delete(to.name as string);
       }
     },
   },
