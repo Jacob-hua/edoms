0938cf85672053578d94758585aab7b10f5e1219
yanheng <yanheng@siactpower.com>
2023-02-02 17:02:36 +0800

    feat: 动态创建页面实例

--- edoms-runtime/page/App.vue
+++ edoms-runtime/page/App.vue
@@ -1,17 +1,23 @@
 <template>
   <edoms-ui-page :config="pageConfig"></edoms-ui-page>
+  <!-- <RouterView></RouterView> -->
 </template>
 
 <script lang="ts" setup>
-import { inject, onMounted, onUnmounted, ref, watch } from 'vue';
+import { h, inject, onMounted, onUnmounted, ref, watch } from 'vue';
+import { RouteRecordRaw } from 'vue-router';
 
 import Core from '@edoms/core';
-import { MApp } from '@edoms/schema';
+import { MApp, MPage } from '@edoms/schema';
 import { getUrlParam } from '@edoms/utils';
 
 const app = inject<Core | undefined>('app');
+
 const pageConfig = ref(app?.page?.data || {});
+
 const dsl = ref<MApp>();
+
+const routes = ref<RouteRecordRaw[]>([]);
 
 onMounted(() => {
   const isLocalPreview = getUrlParam('localPreview');
@@ -36,6 +42,17 @@ watch(
     }
     app.setConfig(dsl, getUrlParam('page') ?? dsl.index);
     pageConfig.value = app.page?.data || {};
+    routes.value = dsl.items.reduce(
+      (routes, page: MPage) => [
+        ...routes,
+        {
+          path: `/${page.id}`,
+          name: page.name,
+          component: h('edoms-ui-page', { config: page, name: page.id }),
+        },
+      ],
+      [] as RouteRecordRaw[]
+    );
   }
 );
 
