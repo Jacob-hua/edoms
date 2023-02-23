c5ddf2fb98413a681f542a22ccce55924e1eb2af
yanheng <yanheng@siactpower.com>
2023-02-23 14:55:13 +0800

    update: 更新多选框的初始值

--- packages/ui/src/checkbox/src/initValue.ts
+++ packages/ui/src/checkbox/src/initValue.ts
@@ -1,6 +1,28 @@
 export default {
+  options: [
+    {
+      label: '选项1',
+      value: '1',
+      disabled: false,
+      selected: false,
+    },
+    {
+      label: '选项2',
+      value: '2',
+      disabled: false,
+      selected: false,
+    },
+    {
+      label: '选项3',
+      value: '3',
+      disabled: false,
+      selected: false,
+    },
+  ],
   style: {
     width: '85',
     height: '33',
+    fontSize: '14',
+    color: 'rgba(255, 255, 255, 1)',
   },
 };
