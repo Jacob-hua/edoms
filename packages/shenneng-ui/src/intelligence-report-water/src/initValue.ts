9bfe1c8fba145876be79bff58ae8bc44199103e0
lihao <lih@siactpower.com>
2023-05-24 17:58:58 +0800

    fix: 1.能效监测组件增加精度校验 2.二级弹窗背景色统一调整

--- packages/shenneng-ui/src/intelligence-report-water/src/initValue.ts
+++ packages/shenneng-ui/src/intelligence-report-water/src/initValue.ts
@@ -3,14 +3,14 @@
  * @Author: lihao
  * @Date: 2023-04-27 11:42:51
  * @LastEditors: lihao
- * @LastEditTime: 2023-05-22 09:34:55
+ * @LastEditTime: 2023-05-24 14:58:07
  */
 export default {
   type: 'intelligence-report-water',
   style: {
     width: '117',
     height: '80',
-    backgroundColor: 'rgba(0, 163, 255, 0.06)',
+    backgroundColor: 'rgba(0, 163, 255, 0.1)',
   },
   title: '智能报告',
 };
