a4ac6f7827b73a1eb0c8f05fc92464629e1f3e7c
liuhua <957404156@qq.com>
2023-06-01 14:52:05 +0800

    feat: 储能运行参数

---
+++ packages/shenneng-ui/src/running-parameter-energy/index.ts
@@ -0,0 +1,7 @@
+import RunningParameterEnergy from './src/index.vue';
+
+export { default as config } from './src/formConfig';
+export { default as value } from './src/initValue';
+export { default as event } from './src/event';
+
+export default RunningParameterEnergy;
---
+++ packages/shenneng-ui/src/running-parameter-energy/src/initValue.ts
@@ -0,0 +1,9 @@
+export default {
+  type: 'running-parameter-energy',
+  title: '运行参数',
+  style: {
+    width: '117',
+    height: '80',
+    backgroundColor: 'rgba(0, 163, 255, 0.06)',
+  },
+};
