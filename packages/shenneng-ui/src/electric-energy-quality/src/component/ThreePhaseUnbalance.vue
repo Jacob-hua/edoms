af7864887c6659c104133215ac8ac1bb0cc1e075
xietao <xt18309297845@163.com>
2023-06-02 13:52:07 +0800

    fix: echarts图例配置

--- packages/shenneng-ui/src/electric-energy-quality/src/component/ThreePhaseUnbalance.vue
+++ packages/shenneng-ui/src/electric-energy-quality/src/component/ThreePhaseUnbalance.vue
@@ -343,8 +343,6 @@ watch(
             align: 'right',
             padding: 7,
           },
-          // data: ['2', '4', '6', '8'],
-          data: ['0', '20', '40', '60', '80', '100'],
           splitLine: {
             lineStyle: {
               type: 'dashed',
