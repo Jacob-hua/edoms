637229c6064e8dacb53c71485883848b702c6c57
yanheng <yanheng@siactpower.com>
2023-06-19 10:10:59 +0800

    feat: 新增useLocales.ts用于组件国际化

---
+++ packages/shenneng-ui/src/useLocales.ts
@@ -0,0 +1,28 @@
+import { inject } from 'vue';
+
+import Core from '@edoms/core';
+
+export const useApp = () => {
+  const app = inject<Core | undefined>('app');
+
+  function t(str: string): string {
+    if (!app) {
+      return str;
+    }
+    return app.t(str);
+  }
+
+  function setMessage(message: Record<string, any>): void {
+    if (!app) {
+      return;
+    }
+    app.setMessage(message);
+  }
+
+  return {
+    t,
+    setMessage,
+  };
+};
+
+export default useApp;
