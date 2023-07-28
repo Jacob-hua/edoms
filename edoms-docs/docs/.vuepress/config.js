9e098e42dd951c79567eef5cecc95a40f8aaf589
xietao <xt18309297845@163.com>
2023-07-28 18:05:44 +0800

    fix: edoms静态资源站点

--- edoms-docs/docs/.vuepress/config.js
+++ edoms-docs/docs/.vuepress/config.js
@@ -1,11 +1,78 @@
-import { defineUserConfig } from 'vuepress';
+import { defineUserConfig, defaultTheme } from 'vuepress';
 
 export default defineUserConfig({
   base: '/',
   lang: 'zh-CN',
-  title: '你好, VuePress',
-  description: '这是我的第一个 VuePress 站点',
+  title: '你好, E-DOMs',
+  description: 'E-DOMs 静态站点',
   host: '0.0.0.0',
   port: 8800,
   open: true,
+  theme: defaultTheme({
+    // 添加侧边栏
+    sidebar: {
+      '/': [
+        {
+          text: '基础组件',
+          collapsible: true,
+          children: [
+            '/component/basiccomponents/text.md',
+            '/component/basiccomponents/button.md',
+            '/component/basiccomponents/picture.md',
+            '/component/basiccomponents/carousel.md',
+            '/component/basiccomponents/QRcode.md',
+            '/component/basiccomponents/checkbox.md',
+            '/component/basiccomponents/chart.md',
+          ],
+        },
+        {
+          text: '基础容器',
+          collapsible: true,
+          children: [
+            '/component/baseContainer/bulletBox.md',
+            '/component/baseContainer/group.md',
+            '/component/baseContainer/smegma.md',
+          ],
+        },
+        {
+          text: '共通组件',
+          collapsible: true,
+          children: [
+            '/component/publicComponents/economic-indicators.md',
+            '/component/publicComponents/energy-monitoring.md',
+            '/component/publicComponents/equipment-condition.md',
+            '/component/publicComponents/equipment-query-operation.md',
+            '/component/publicComponents/global-schematic.md',
+            '/component/publicComponents/image-light.md',
+            '/component/publicComponents/operation-records.md',
+            '/component/publicComponents/operational-analysis.md',
+            '/component/publicComponents/polymorphic-light.md',
+            '/component/publicComponents/running-parameter-energy.md',
+            '/component/publicComponents/single-light.md',
+            '/component/publicComponents/suspended-window.md',
+            '/component/publicComponents/system-data-cumulation.md',
+            '/component/publicComponents/warning-table-list.md',
+          ],
+        },
+        {
+          text: '业务组件',
+          collapsible: true,
+          children: [
+            '/component/businessComponents/asset-information.md',
+            '/component/businessComponents/control-charge.md',
+            '/component/businessComponents/control-panel.md',
+            '/component/businessComponents/efficiency-analysis.md',
+            '/component/businessComponents/electric-energy-quality.md',
+            '/component/businessComponents/equipment-monitoring.md',
+            '/component/businessComponents/fee-setting-energy.md',
+            '/component/businessComponents/intelligence-report.md',
+            '/component/businessComponents/load-forecasting.md',
+            '/component/businessComponents/operations-analysis.md',
+            '/component/businessComponents/optimal-regulation.md',
+            '/component/businessComponents/running-parameter.md',
+          ],
+        },
+      ],
+    },
+  })
 });
--- edoms-docs/docs/README.md
+++ edoms-docs/docs/README.md
@@ -1,1 +1,18 @@
-# 你好
+---
+home: true
+heroImage: /favicon.ico
+heroText: Welcome to E-DOMs.
+tagline: Born to win.
+actions: 
+  - text: 开启探索之旅→
+    link: /component/
+    type: primary
+features:
+- title: 关于我
+  details: E-DOMs是一个低代码开发平台...
+- title: 算法
+  details: 我也不知道E-DOMs有哪些算法,只是占个位置罢了。
+- title: 生活点点滴滴
+  details: 记录生活中美好的时刻，把生命中的点点滴滴都堆积成无法比拟的幸福。
+footer: E-DOMs | Copyright © 2023-present JasonJing
+---
