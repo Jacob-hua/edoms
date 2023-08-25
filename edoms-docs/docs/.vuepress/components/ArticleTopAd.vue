f026609d484adadc76a83aab0d7522a19737d9fe
xietao <xt18309297845@163.com>
2023-08-25 17:22:30 +0800

    fix: markdown文档组件传值

--- edoms-docs/docs/.vuepress/components/ArticleTopAd.vue
+++ edoms-docs/docs/.vuepress/components/ArticleTopAd.vue
@@ -1,7 +1,20 @@
 <template>
-    <div>这是自定义的组件</div>
+    <div>
+        <div>
+            <button @click="sendMassage">表单</button>
+        </div>
+        <m-form></m-form>
+        <Preview :option="dataVal"></Preview>
+    </div>
 </template>
   
-<script>
-export default {};
+<script lang="ts" setup>
+import { ref } from 'vue';
+import type { MForm } from '@edoms/form';
+import Preview from './Preview.vue';
+const dataVal = ref<number>(0)
+const sendMassage = (() => {
+    dataVal.value = dataVal.value + 1;
+})
+
 </script>
---
+++ edoms-docs/docs/.vuepress/components/Preview.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        预览: 表单按钮被点击了{{ props.option }}次
+    </div>
+</template>
+<script setup lang="ts">
+
+const props = defineProps<{
+    option: number;
+}>();
+</script>
