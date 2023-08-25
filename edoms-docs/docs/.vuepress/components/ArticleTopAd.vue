49aeca6b5a80cf7cdcfb9f10a0f42f5c2f4efb2c
xietao <xt18309297845@163.com>
2023-08-25 17:23:55 +0800

    fix: markdown文档静态资源组件传值

--- edoms-docs/docs/.vuepress/components/ArticleTopAd.vue
+++ edoms-docs/docs/.vuepress/components/ArticleTopAd.vue
@@ -3,14 +3,12 @@
         <div>
             <button @click="sendMassage">表单</button>
         </div>
-        <m-form></m-form>
         <Preview :option="dataVal"></Preview>
     </div>
 </template>
   
 <script lang="ts" setup>
 import { ref } from 'vue';
-import type { MForm } from '@edoms/form';
 import Preview from './Preview.vue';
 const dataVal = ref<number>(0)
 const sendMassage = (() => {
