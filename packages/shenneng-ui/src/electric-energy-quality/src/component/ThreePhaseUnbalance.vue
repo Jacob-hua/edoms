225960dd7a71c9ff4982624382e7b3c985c251f1
yanheng <yanheng@siactpower.com>
2023-10-16 17:36:25 +0800

    update: 更新三相不平衡组件逻辑

--- packages/shenneng-ui/src/electric-energy-quality/src/component/ThreePhaseUnbalance.vue
+++ packages/shenneng-ui/src/electric-energy-quality/src/component/ThreePhaseUnbalance.vue
@@ -35,13 +35,22 @@
       </div>
     </div>
     <div class="dataEchart_second">
-      <ProportionChart :option="option_prop"> </ProportionChart>
+      <div class="wrapper">
+        <div class="echart">{{ t('占比分布') }}</div>
+        <EdomsCharts class="charts" :option="proportionDistributionConfig([60.17, 19.56, 15.42, 0.85])"></EdomsCharts>
+      </div>
     </div>
     <div style="margin: 20px">
-      <CurrentChart :option="item.name === 'four' ? option_current4 : option_current5"></CurrentChart>
+      <div class="wrapper">
+        <div class="echart">{{ t('电流') }}</div>
+        <EdomsCharts class="charts" :option="currentCurveConfig([], [], [])"></EdomsCharts>
+      </div>
     </div>
     <div style="margin: 20px">
-      <LoadChart :option="item.name === 'four' ? option_load4 : option_load5"></LoadChart>
+      <div class="wrapper">
+        <div class="echart" style="width: 485px">{{ t('负载率') }}</div>
+        <EdomsCharts class="charts" :option="loadCurveConfig([], [])"></EdomsCharts>
+      </div>
     </div>
   </div>
 </template>
@@ -49,25 +58,267 @@
 <script lang="ts" setup>
 import { ref, watch } from 'vue';
 
-// import useIntervalAsync from '../../../useIntervalAsync';
+import EdomsCharts from '../../../EdomsCharts.vue';
 import { ECOption } from '../../../types';
+import useApp from '../../../useApp';
 import useI18n from '../../../useI18n';
 import { ElectricEnergyQuality } from '../type';
 
-import CurrentChart from './CurrentChart.vue';
-import LoadChart from './LoadChart.vue';
-// import useApp from '../../../useApp';
-import ProportionChart from './ProportionChart.vue';
-const { t } = useI18n();
 const props = defineProps<{
   config: ElectricEnergyQuality;
 }>();
-// const byq4_current = {
-//   Ia: [291.2841, 279.9651, 248.0136, 277.4367, 249.8106, 248.2334, 249.5328, 307.9369],
-//   Ib: [292.6177, 280.9302, 247.5605, 276.8418, 250.9959, 247.2667, 249.6436, 308.0461],
-//   Ic: [292.6016, 282.2231, 248.5004, 277.256, 251.6891, 247.5361, 250.0867, 306.9834],
-// };
-// const { request } = useApp(props);
+
+useApp(props);
+
+const { t } = useI18n();
+
+const proportionDistributionConfig = (data: any): ECOption => ({
+  legend: {
+    top: '5%',
+    left: 'center',
+    itemWidth: 8,
+    itemHeight: 8,
+    textStyle: {
+      color: '#fff',
+    },
+  },
+  color: props.config.proportion.map(({ color }) => color),
+  series: [
+    {
+      name: 'Access From',
+      type: 'pie',
+      radius: ['40%', '65%'],
+      top: 40,
+      label: {
+        show: true,
+        position: 'outside',
+        formatter: '{d}%',
+        color: '#fff',
+      },
+      labelLine: {
+        show: true,
+        length: 5,
+        length2: 5,
+      },
+      data: [
+        { value: data[0], name: '<15%' },
+        { value: data[1], name: '15%-30%' },
+        { value: data[2], name: '30%-50%' },
+        { value: data[3], name: '>50%' },
+      ],
+    },
+  ],
+});
+
+const currentCurveConfig = (ia: any, ib: any, ic: any): ECOption => ({
+  tooltip: {
+    trigger: 'axis',
+    backgroundColor: 'rgba(11,34,52,0.9)',
+    borderColor: '#204C6F',
+    borderWidth: 1,
+    formatter: (params: any) => {
+      let tip: string = '';
+      if (params != null && params.length > 0) {
+        tip +=
+          '<div style="min-width:105px;height:90px"><span style="margin-left:8px;color:#C4E5F8;font-size:12px;font-weight: 400;line-height:18px">' +
+          t('三相电流') +
+          '</span><br />';
+        for (let index = 0; index < params.length; index++) {
+          tip +=
+            '<span style="margin-left:8px;color:#C4E5F8;font-size:12px;font-weight: 400;line-height:18px">' +
+            params[index].seriesName +
+            ':</span><span style="line-height:18px;margin-left:8px;color:' +
+            params[index].color +
+            '">' +
+            params[index].value +
+            'A</span> <br />';
+        }
+        tip += '</div>';
+      }
+      return tip;
+    },
+  },
+  legend: {
+    data: ['Ia', 'Ib', 'Ic'],
+    textStyle: {
+      color: '#fff',
+    },
+  },
+  xAxis: {
+    type: 'category',
+    data: ['1', '2', '3', '4', '5', '6', '7', '8'],
+    axisTick: {
+      show: false,
+    },
+  },
+  yAxis: {
+    type: 'value',
+    name: 'A',
+    nameTextStyle: {
+      align: 'right',
+      padding: 7,
+    },
+    splitLine: {
+      lineStyle: {
+        type: 'dashed',
+        color: '#1A242B',
+        width: 1,
+      },
+    },
+  },
+  grid: { top: '35px', left: '30px', right: '30px', bottom: '44px' },
+  color: [props.config.currentLa, props.config.currentLb, props.config.currentLc],
+  series: [
+    {
+      name: 'Ia',
+      // data: [291.2841, 279.9651, 248.0136, 277.4367, 249.8106, 248.2334, 249.5328, 307.9369],
+      data: ia,
+      type: 'line',
+      smooth: true,
+      symbolSize: 0,
+    },
+    {
+      name: 'Ib',
+      // data: [292.6177, 280.9302, 247.5605, 276.8418, 250.9959, 247.2667, 249.6436, 308.0461],
+      data: ib,
+      type: 'line',
+      smooth: true,
+      symbolSize: 0,
+    },
+    {
+      name: 'Ic',
+      // data: [292.6016, 282.2231, 248.5004, 277.256, 251.6891, 247.5361, 250.0867, 306.9834],
+      data: ic,
+      type: 'line',
+      smooth: true,
+      symbolSize: 0,
+    },
+  ],
+});
+
+const loadCurveConfig = (loadRate: any, imbalanceRate: any): ECOption => ({
+  tooltip: {
+    trigger: 'axis',
+    backgroundColor: 'rgba(11,34,52,0.9)',
+    borderColor: '#204C6F',
+    borderWidth: 1,
+    padding: 15,
+    formatter: (params: any) => {
+      let tip: string = '';
+      if (params != null && params.length > 0) {
+        tip +=
+          '<div style="min-width: 130px;height: 45px"><span style="margin-left:8px;color:#C4E5F8;font-size:12px;font-weight: 400;line-height:18px">' +
+          t('负载率') +
+          '</span><br />';
+        for (let index = 0; index < params.length; index++) {
+          tip +=
+            '<p style="width:100%"><span style="color:#F5F7FA;font-size:12px;font-weight:400">' +
+            params[index].seriesName +
+            ':</span><span style="margin-left:8px;color:' +
+            params[index].color +
+            ';font-size:12px;font-weight:400">' +
+            params[index].value +
+            '%</span></p>';
+        }
+        tip += '</div>';
+      }
+      return tip;
+    },
+  },
+  legend: {
+    data: [t('负载率'), t('三相不平衡率')],
+    textStyle: {
+      color: '#fff',
+    },
+  },
+  xAxis: {
+    type: 'category',
+    data: ['1', '2', '3', '4', '5', '6', '7', '8'],
+    axisTick: {
+      show: false,
+    },
+  },
+  yAxis: [
+    {
+      type: 'value',
+      name: '%',
+      nameTextStyle: {
+        align: 'right',
+        padding: 7,
+      },
+      splitLine: {
+        lineStyle: {
+          type: 'dashed',
+          color: '#1A242B',
+          width: 1,
+        },
+      },
+    },
+    {
+      type: 'category',
+      name: '%',
+      nameTextStyle: {
+        align: 'left',
+        padding: 7,
+      },
+      axisLine: {
+        show: false,
+      },
+      axisTick: {
+        show: false,
+      },
+      data: ['0', '20', '40', '60', '80', '100'],
+      splitLine: {
+        lineStyle: {
+          type: 'dashed',
+          color: '#1A242B',
+          width: 1,
+        },
+      },
+    },
+  ],
+  grid: { top: '35px', left: '35px', right: '35px', bottom: '44px' },
+  color: [props.config.loadRate, props.config.threePhasRate],
+  series: [
+    {
+      name: t('负载率'),
+      // data: [40.12128769, 33.735780675, 42.592085488, 36.602015599, 36.8932692, 35.80097567, 46.652001007, 42.6578128],
+      data: loadRate,
+      type: 'line',
+      smooth: true,
+      symbolSize: 0,
+    },
+    {
+      name: t('三相不平衡率'),
+      // data: [15.3978635, 22.1156983, 19.1727799, 19.3278177, 26.1765207, 22.3943031, 13.307268, 12.6976074],
+      data: imbalanceRate,
+      type: 'line',
+      smooth: true,
+      symbolSize: 0,
+      areaStyle: {
+        color: {
+          type: 'linear',
+          x: 0,
+          y: 0,
+          x2: 0,
+          y2: 1,
+          colorStops: [
+            {
+              offset: 0,
+              color: 'rgba(40, 124, 232, 0.16)', // 0% 处的颜色
+            },
+            {
+              offset: 1,
+              color: 'rgba(40, 124, 232, 0)', // 100% 处的颜色
+            },
+          ],
+          global: false, // 缺省为 false
+        },
+      },
+    },
+  ],
+});
+
 // 占比分布
 const option_prop = ref<ECOption>({});
 // 电流
@@ -76,24 +327,19 @@ const option_current5 = ref<ECOption>({});
 // 负载率
 const option_load4 = ref<ECOption>({});
 const option_load5 = ref<ECOption>({});
+
 // 获取饼图颜色
 const getpropColor = (series: any[] = []) => {
   return series.map(({ color }) => color);
 };
-// 获取图例名称
-// const getpropName = (series: any[] = []) => {
-//   return series.map(({ label }) => label);
-// };
 
 //数据配置不详 后续在此数据解析
 watch(
   () => props.config.proportion,
   () => {
-    console.log(props.config);
     const propColor = getpropColor(props.config.proportion);
     option_prop.value = {
       legend: {
-        // data: propName,
         top: '5%',
         left: 'center',
         itemWidth: 8,
@@ -173,13 +419,10 @@ watch(
       yAxis: {
         type: 'value',
         name: 'A',
-        // nameGap: 15,
-        // offset: 15,
         nameTextStyle: {
           align: 'right',
           padding: 7,
         },
-        // data: ['2', '4', '6', '8'],
         splitLine: {
           lineStyle: {
             type: 'dashed',
@@ -258,13 +501,10 @@ watch(
       yAxis: {
         type: 'value',
         name: 'A',
-        // nameGap: 15,
-        // offset: 15,
         nameTextStyle: {
           align: 'right',
           padding: 7,
         },
-        // data: ['2', '4', '6', '8'],
         splitLine: {
           lineStyle: {
             type: 'dashed',
@@ -342,8 +582,6 @@ watch(
         {
           type: 'value',
           name: '%',
-          // nameGap: 15,
-          // offset: 15,
           nameTextStyle: {
             align: 'right',
             padding: 7,
@@ -359,8 +597,6 @@ watch(
         {
           type: 'category',
           name: '%',
-          // nameGap: 15,
-          // offset: 15,
           nameTextStyle: {
             align: 'left',
             padding: 7,
@@ -578,7 +814,6 @@ const categories = ref([
     name: 'five',
   },
 ]);
-// useIntervalAsync(updateEfficiencyData, intervalDelay.value);
 </script>
 
 <style lang="scss" scoped>
@@ -660,5 +895,18 @@ const categories = ref([
     margin: 20px 0px;
     border-right: 1px solid #1d2634;
   }
+}
+
+.echart {
+  margin-left: 22px;
+  font-size: 14px;
+  font-family: Microsoft YaHei;
+  font-weight: 400;
+  color: #eaf5ff;
+}
+
+.charts {
+  width: 487px;
+  height: 276px;
 }
 </style>
